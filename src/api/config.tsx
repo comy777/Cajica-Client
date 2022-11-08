69ab754c7d341b0e1caa2cab0528f5833b3b1f6d
Sebastian Carrillo <carrillosebastian1@gmail.com>
2022-11-08 15:23:29 -0500

    Added zoom to images

--- src/api/config.tsx
+++ src/api/config.tsx
@@ -4,5 +4,5 @@ const baseURL = "https://reci-cajica-server.herokuapp.com/";
 const localhost = "http://192.168.100.9:5050";
 
 export const apiCajica = axios.create({
-  baseURL: localhost,
+  baseURL,
 });
